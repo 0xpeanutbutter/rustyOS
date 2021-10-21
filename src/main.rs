e0e8b6e053f39aa31cd98c92e36414881ca2f504
omar1024 <omar1024@protonmail.com>
2021-10-21 21:45:20 +0530

    nostd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+//disabling standard lib
+#![no_std]
+
+//imports
+use core::panic::PanicInfo;
+
+// function called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) ->!{
+    loop{}
+}
 fn main() {
-    println!("Hello, world!");
 }
